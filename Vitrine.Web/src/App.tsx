6ae98ca1c6f2d4501b3bd6547d424b526c71e637
Flavio Assis <flavioh.dev@gmail.com.br>
2022-01-24 08:20:23 -0300

    Não funcional

--- Vitrine.Web/src/App.tsx
+++ Vitrine.Web/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import Header from './components/Header';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+	return (
+		<div className="App">
+			<Header />
+			{/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,9 +20,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-    </div>
-  );
+      </header> */}
+		</div>
+	);
 }
 
 export default App;
---
+++ Vitrine.Web/src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.css';
+
+const Header = () => (
+	<section id='header'>
+		<h1>Vitrine dos Produtos</h1>
+	</section>
+);
+
+export default Header;
