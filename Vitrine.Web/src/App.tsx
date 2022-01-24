d930e5ae22a67f5b0f4d5e9a2a61b85422b330ff
Flavio Assis <flavioh.assis@lambda3.com.br>
2022-01-24 13:36:25 -0300

    Criando o Header

--- Vitrine.Web/src/App.tsx
+++ Vitrine.Web/src/App.tsx
@@ -2,26 +2,13 @@ import React from 'react';
 
 import Header from './components/Header';
 import './App.css';
+import './assets/global.css';
 
 function App() {
 	return (
-		<div className="App">
+		<section className="App">
 			<Header />
-			{/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-		</div>
+		</section>
 	);
 }
 
--- Vitrine.Web/src/components/Header/index.tsx
+++ Vitrine.Web/src/components/Header/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+
+import Logotipo from '../../assets/images/nome.png';
 import './styles.css';
 
 const Header = () => (
 	<section id='header'>
-		<h1>Vitrine dos Produtos</h1>
+		<h1 id='title'>Vitrine dos Produtos</h1>
+		<img id='nome' src={Logotipo} alt='Emiradus' />
 	</section>
 );
 
